06e45a2d2e9986725e055c8d85e03ded20b7c220
Muroko <murokomail@gmail.com>
2024-01-22 23:39:46 -0500

    UPDATE: new contract address set for testing

--- dapp.config.js
+++ dapp.config.js
@@ -1,11 +1,11 @@
 const config = {
   title: 'Dnero Gold NFT Dapp',
-  description: 'A showcase dapp which is built for you to create your own',
+  description: 'nero GoldNFT are a collection of 100,000 burn to redeem NFTs to be paired to real life Gold.',
   //contractAddress: '0x0312e42e4b55823a3C41769DC0B07F382dECc247', //Dnero Mainnet
-  contractAddress: '0x24911a4bd0b09228f73fd3bbc65f09f090f8bb25', //GoldNft-Test
+  contractAddress: '0xbbc068fad6219add750679fb5eb2dc0c4a077475', //GoldNft-Test
   maxMintAmount: 100000,
   presaleMaxMintAmount: 1000,
-  price: 10.00
+  price: 0.0001
 }
 
 export { config }
